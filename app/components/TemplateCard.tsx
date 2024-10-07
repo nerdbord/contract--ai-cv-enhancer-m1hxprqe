ef25c88cdacb46928b68d03ccb6f92848391a8fc
Mateusz <matmatwiej@gmail.com>
2024-10-07 14:49:25 +0200

    feat: create second form step (#2)

---
+++ app/components/TemplateCard.tsx
@@ -0,0 +1,25 @@
+﻿import { cn } from "~/lib/utils";
+
+type TemplateCardProps = {
+  onChoose: () => void;
+  choosen: boolean;
+  title: string;
+  imgSrc: string;
+  alt: string;
+};
+
+export const TemplateCard = ({ onChoose, choosen, title, imgSrc, alt }: TemplateCardProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <p className="text-center text-black">{title}</p>
+
+      <button
+        type="button"
+        onClick={onChoose}
+        className={cn("transition duration-300", choosen ? "border-2 border-violet-900" : "")}
+      >
+        <img src={imgSrc} alt={alt} />
+      </button>
+    </div>
+  );
+};
---
+++ app/components/TemplateStep.tsx
@@ -0,0 +1,42 @@
+﻿import { useState } from "react";
+import { TemplateCard } from "~/components/TemplateCard";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+
+export type CVTemplateName = "classic" | "modern";
+
+export const TemplateStep = () => {
+  const [cvStyle, setCvStyle] = useState<CVTemplateName | null>(null);
+
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <h1 className="text-center text-3xl font-semibold text-slate-900">
+        Wybierz szablon, którego mamy użyć do wygenerowania Twojego CV
+      </h1>
+
+      <div className="grid grid-cols-2 gap-5">
+        <TemplateCard
+          onChoose={() => setCvStyle("classic")}
+          choosen={cvStyle === "classic"}
+          imgSrc="/public/cv-templates/classic.png"
+          alt="Classic CV template"
+          title="Klasyczny"
+        />
+
+        <TemplateCard
+          onChoose={() => setCvStyle("modern")}
+          choosen={cvStyle === "modern"}
+          imgSrc="/public/cv-templates/modern.png"
+          alt="Modern CV template"
+          title="Nowoczesny"
+        />
+      </div>
+
+      <Input type="hidden" name="cvStyle" value={cvStyle || ""} />
+
+      <Button type="submit" disabled={!cvStyle}>
+        Dodaj link do ogłoszenia
+      </Button>
+    </div>
+  );
+};
