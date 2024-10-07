e8c381f0d84afeaa29a52bd628a810a91f11e1aa
Mateusz <matmatwiej@gmail.com>
2024-10-07 16:16:59 +0200

    feat: create summary step (#4)

---
+++ app/components/SummaryStep.tsx
@@ -0,0 +1,22 @@
+﻿import { Button } from "~/components/ui/button";
+import { ActionData } from "~/routes/_index";
+
+type SummaryStepProps = {
+  summary: ActionData | undefined;
+};
+
+export const SummaryStep = ({ summary }: SummaryStepProps) => {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <h1 className="text-center text-3xl font-semibold text-slate-900">
+        Sprawdź, jak dopasowaliśmy Twoje CV, i, jeśli chcesz, edytuj je dalej
+      </h1>
+
+      <Button disabled={!summary} type="button">
+        Pobierz
+      </Button>
+
+      {!summary ? "skeleton" : <div>wygenerowane cv</div>}
+    </div>
+  );
+};
