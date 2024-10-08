ccef7fd7d5d6731fc50393cb975b4d6865ab6bd6
transcendence12 <gosiek.krawczuk@gmail.com>
2024-10-08 14:39:08 +0200

    style: second column

--- app/components/TemplateCV.tsx
+++ app/components/TemplateCV.tsx
@@ -59,7 +59,7 @@ export const TemplateCV: React.FC<TemplateCVProps> = ({ data }) => {
       {/* PIERWSZA-KOLUMNA */}
       <div className="flex max-w-56 flex-col gap-5">
         {/* Personal Information */}
-        <h1 className="text-2xl font-semibold">{data.name}</h1>
+        <h1 className="text-2xl font-semibold leading-6">{data.name}</h1>
         <section className="">
           <h3 className="gap-2 text-xs font-bold">KONTAKT</h3>
           <p>{data.contact.email}</p>
@@ -105,48 +105,54 @@ export const TemplateCV: React.FC<TemplateCVProps> = ({ data }) => {
 
         {/* Clause */}
         <section>
-          <h3 className="gap-2 text-xs font-bold">KLAUZULA</h3>
-          <p>
-            Wyrażam zgode na przetwarzanie moich danych osobowych przez {data.company} w celu
-            prowadzenia rekrutacji na aplikowane przeze mnie stanowisko.
+          <h3 className="mb-2 text-xs font-bold">KLAUZULA</h3>
+          <p className="text-[8px] font-normal text-[#474F53]">
+            Wyrażam zgode na przetwarzanie moich danych osobowych przez{" "}
+            <span className="text-[8px] font-black">{data.company}</span> w celu prowadzenia
+            rekrutacji na aplikowane przeze mnie stanowisko.
           </p>
         </section>
       </div>
-
-      <div className="DRUGA-KOLUMNA">
-        <h2 className="text-base font-normal">CV - {data.positionTitle}</h2>
+      {/* DRUGA-KOLUMNA */}
+      <div className="flex max-w-[342px] flex-col gap-5">
+        <h2 className="text-base font-normal text-[#838994]">CV - {data.positionTitle}</h2>
         {/* Summary */}
-        <section className="">
-          <h3 className="gap-2 text-xs font-bold">O MNIE</h3>
-          <p className="text-#474F53">{data.summary}</p>
+        <section className="mb-6">
+          <h3 className="mb-2 gap-2 text-xs font-bold">O MNIE</h3>
+          <p className="text-xs font-normal text-[#474F53]">{data.summary}</p>
         </section>
 
         {/* Experience */}
-        <section className="">
-          <h3 className="gap-2 text-xs font-bold">DOŚWIADCZENIE</h3>
+        <section className="mb-6">
+          <h3 className="mb-4 gap-2 text-xs font-bold">DOŚWIADCZENIE</h3>
           {data.experience.map((job, index) => (
             <div key={index}>
-              <p className="text-#838994 text-xs font-normal">
-                {job.company} {job.companyType} {job.sector}
+              <p className="mb-1 flex gap-1 text-xs font-normal text-[#838994]">
+                <span className="underline">{job.company}</span>
+                <span>{job.companyType}</span>
+                <span>{job.sector}</span>
               </p>
-              <h4 className="text-base font-semibold">
-                {job.position}{" "}
-                <span className="text-#838994 text-xs font-normal">{job.duration}</span>
+              <h4 className="flex items-center justify-between text-base font-semibold">
+                <span>{job.position}</span>
+                <span className="text-xs font-normal text-[#838994]">{job.duration}</span>
               </h4>
-              <p className="text-#474F53">{job.description}</p>
+              <p className="mb-6 text-xs font-normal text-[#474F53]">{job.description}</p>
             </div>
           ))}
         </section>
 
         {/* Education */}
-        <section className="">
-          <h3 className="gap-2 text-xs font-bold">WYKSZTAŁCENIE</h3>
+        <section className="mb-6">
+          <h3 className="mb-2 text-xs font-bold">WYKSZTAŁCENIE</h3>
           {data.education.map((edu, index) => (
             <div key={index}>
-              <h4>{edu.institution}</h4>
-              <p>
-                {edu.degree} i {edu.fieldOfStudy} <span>{edu.duration}</span>
-              </p>
+              <h4 className="mb-1 text-xs font-normal text-[#838994]">{edu.institution}</h4>
+              <div className="flex items-center justify-between">
+                <p className="text-base font-normal">
+                  {edu.degree} i {edu.fieldOfStudy}
+                </p>
+                <span className="text-xs font-normal text-[#838994]">{edu.duration}</span>
+              </div>
             </div>
           ))}
         </section>
