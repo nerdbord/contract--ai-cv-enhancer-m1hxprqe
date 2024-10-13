3b63932d02b0f2ea5d45fbd443147d043fcb74b0
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-10-13 22:40:23 +0200

    refactor: refactor

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,34 +1,15 @@
-import {
-  SignInButton,
-  SignOutButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-  useUser,
-} from "@clerk/remix";
-import { useEffect } from "react";
+import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/remix";
 
 export default function Header() {
-  const { isLoaded, isSignedIn, user } = useUser();
-  console.log("USER: ", isLoaded, isSignedIn, user);
-
-  useEffect(() => {
-    console.log("USER: ", isLoaded, isSignedIn, user);
-    if (isLoaded && isSignedIn && user) {
-      console.log("User email: ", user.emailAddresses[0].emailAddress);
-    }
-  }, [isLoaded, isSignedIn, user]);
   return (
     <header className="flex h-[100px] w-full items-center justify-end px-8">
       <nav className="flex items-center justify-end gap-4">
         <SignedIn>
-          <p>Welcome, {user?.emailAddresses[0]?.emailAddress}!</p>
-          <div>
-            <UserButton />
-          </div>
-          <div>
-            <SignOutButton />
-          </div>
+          <SignOutButton>
+            <button className="flex items-center justify-between gap-[10px] rounded-md border-2 border-slate-800 px-11 py-2">
+              Wyloguj
+            </button>
+          </SignOutButton>
         </SignedIn>
         <SignedOut>
           <SignInButton mode="modal">
--- app/components/SummaryStep.tsx
+++ app/components/SummaryStep.tsx
@@ -1,5 +1,4 @@
-﻿import { SignedIn, SignedOut, SignIn, useSignIn, useUser } from "@clerk/remix";
-import { useEffect } from "react";
+﻿import { useUser } from "@clerk/remix";
 import { Button } from "~/components/ui/button";
 import { ActionData } from "~/routes/_index";
 
@@ -8,33 +7,10 @@ type SummaryStepProps = {
 };
 
 export const SummaryStep = ({ summary }: SummaryStepProps) => {
-  const { isLoaded, isSignedIn, user } = useUser();
-  const { signIn } = useSignIn();
+  const { isSignedIn } = useUser();
 
-  // Save CV to localStorage when enhancedCV is available
-  useEffect(() => {
-    if (isLoaded && isSignedIn && user) {
-      console.log("USER: ", isLoaded, isSignedIn, user);
-      console.log("User email: ", user.emailAddresses[0].emailAddress);
-    }
-    if (summary?.enhancedCV) {
-      localStorage.setItem("enhancedCV", summary.enhancedCV);
-    }
-  }, [summary, isLoaded, isSignedIn, user]);
-
-  const handleDownload = async () => {
-    const cv = localStorage.getItem("enhancedCV");
-    if (!isSignedIn) {
-      // Trigger Clerk sign-in and redirect back after login
-
-      return <SignIn />;
-    }
-
-    if (cv) {
-      console.log("Downloading CV: ", cv);
-    } else {
-      console.error("No CV found in localStorage.");
-    }
+  const handleDownload = () => {
+    console.log("Downloading CV...");
   };
 
   return (
@@ -43,27 +19,9 @@ export const SummaryStep = ({ summary }: SummaryStepProps) => {
         Sprawdź, jak dopasowaliśmy Twoje CV, i, jeśli chcesz, edytuj je dalej
       </h1>
 
-      <SignedIn>
-        {/* Only enable the button when the client has mounted and user is signed in */}
-        <Button
-          disabled={!isLoaded || !summary || !isSignedIn}
-          type="button"
-          onClick={handleDownload}
-        >
-          Pobierz
-        </Button>
-      </SignedIn>
-
-      <SignedOut>
-        <p>Musisz się zalogować, aby pobrać CV</p>
-        <Button onClick={() => signIn?.authenticateWithRedirect}>
-          <SignedOut>
-            <div>
-              <SignIn />
-            </div>
-          </SignedOut>
-        </Button>
-      </SignedOut>
+      <Button disabled={!summary || !isSignedIn} type="button" onClick={handleDownload}>
+        Pobierz
+      </Button>
 
       {!summary ? "skeleton" : <div>wygenerowane cv</div>}
     </div>
